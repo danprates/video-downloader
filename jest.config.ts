217a812a86fe0f22bfdfe7da6dd13aa6a062c0ca
Daniel Prates <danprates94@gmail.com>
2022-07-26 22:58:36 -0300

    chore: add tests folder to jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -4,16 +4,20 @@ const tsJest = require('ts-jest/jest-preset')
 export default {
   ...tsJest,
   clearMocks: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  roots: ['<rootDir>/tests'],
+  roots: ['<rootDir>'],
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
-  testMatch: ['<rootDir>/tests/**/*.unit.ts', '<rootDir>/tests/**/*.integration.ts'],
+  testMatch: [
+    '<rootDir>/tests/**/*.unit.ts',
+    '<rootDir>/tests/**/*.integration.ts'
+  ],
   moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
     '@/(.*)': '<rootDir>/src/$1'
   }
-};
+}
