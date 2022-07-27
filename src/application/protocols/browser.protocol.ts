6724ade1860802dffa4f26fac708caa9eba5a26e
Daniel Prates <danprates94@gmail.com>
2022-07-26 22:26:34 -0300

    feat: add browser to protocols

---
+++ src/application/protocols/browser.protocol.ts
@@ -0,0 +1,36 @@
+export interface OpenBrowser {
+  open(): Promise<Browser>
+}
+
+export interface CloseBrowser {
+  close(): Promise<void>
+}
+
+export interface VisitUrl {
+  visit(url: string): Promise<void>
+}
+
+export interface WaitFor {
+  waitFor(selector: string): Promise<void>
+}
+
+export interface Click {
+  click(selector: string): Promise<void>
+}
+
+export interface Type {
+  type(selector: string, value: string): Promise<void>
+}
+
+export interface GetTextContent {
+  getTextContent(selector: string): Promise<string | null>
+}
+
+export interface Browser
+  extends OpenBrowser,
+    CloseBrowser,
+    VisitUrl,
+    WaitFor,
+    Click,
+    Type,
+    GetTextContent {}
