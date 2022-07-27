fcda998b98a9431f905a234a983f965897c33c9f
Daniel Prates <danprates94@gmail.com>
2022-07-26 22:59:10 -0300

    feat: implement Browser interface to PuppeteerAdapter

---
+++ src/infra/browser/puppeteer.adapter.ts
@@ -0,0 +1,45 @@
+import puppeteer from 'puppeteer'
+
+import { Browser } from 'application/protocols'
+
+export class PuppeteerAdapter implements Browser {
+  private constructor(
+    private readonly browser: puppeteer.Browser,
+    private readonly page: puppeteer.Page
+  ) {}
+  open(): Promise<Browser> {
+    throw new Error('Method not implemented.')
+  }
+  static async open(): Promise<Browser> {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    page.setDefaultNavigationTimeout(50000)
+    await page.setViewport({
+      width: 1920,
+      height: 1080
+    })
+    return new PuppeteerAdapter(browser, page)
+  }
+  async close(): Promise<void> {
+    await this.browser.close()
+  }
+  async visit(url: string): Promise<void> {
+    await this.page.goto(url, { waitUntil: 'networkidle2' })
+  }
+  async waitFor(selector: string): Promise<void> {
+    await this.page.waitForSelector(selector)
+  }
+  async click(selector: string): Promise<void> {
+    await this.page.click(selector)
+  }
+  async type(selector: string, value: string): Promise<void> {
+    await this.page.focus(selector)
+    await this.page.keyboard.type(value)
+  }
+  async getTextContent(selector: string): Promise<string | null> {
+    const content = this.page.evaluate(
+      () => document.querySelector(selector)?.textContent
+    )
+    return String(content) || null
+  }
+}
