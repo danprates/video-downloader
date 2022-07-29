bd56871d459103dae3a3b0ac8a7b6d8acb97290f
Daniel Prates <danprates94@gmail.com>
2022-07-29 16:15:52 -0300

    refactor: move sut creation to a factory method

--- tests/application/use-cases/login.use-case.unit.ts
+++ tests/application/use-cases/login.use-case.unit.ts
@@ -1,13 +1,19 @@
 import { LoginUseCase } from '@/application/use-cases'
 import { browserMock } from '@/tests/helpers/mocks'
 
+const makeSut = () => {
+  const sut = new LoginUseCase(browserMock)
+  const params = { email: 'any_email@exemple.com', password: 'any_password' }
+  return {
+    sut,
+    params
+  }
+}
+
 describe('LoginUseCase', () => {
   it('should use browser with correct params', async () => {
-    const sut = new LoginUseCase(browserMock)
-    await sut.execute({
-      email: 'any_email@exemple.com',
-      password: 'any_password'
-    })
+    const { sut, params } = makeSut()
+    await sut.execute(params)
     expect(browserMock.visit).toHaveBeenNthCalledWith(
       1,
       'https://www.vuemastery.com/'
@@ -23,12 +29,12 @@ describe('LoginUseCase', () => {
     expect(browserMock.type).toHaveBeenNthCalledWith(
       1,
       'input[placeholder="Account Email"]',
-      'any_email@exemple.com'
+      params.email
     )
     expect(browserMock.type).toHaveBeenNthCalledWith(
       2,
       'input[placeholder="Password"]',
-      'any_password'
+      params.password
     )
     expect(browserMock.click).toHaveBeenNthCalledWith(
       2,
